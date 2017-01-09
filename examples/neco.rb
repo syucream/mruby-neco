b4a5d0ac859dc539f069cbc85ff93e36ba4c796b
Ryo Okubo <syucream1031@gmail.com>
2017-01-09 14:13:12 +0900

    Add a example for k2hash. NOTE: It works at only Linux

--- examples/neco.rb
+++ examples/neco.rb
@@ -3,10 +3,12 @@ org = Neco::KvsOrg.new
 org.add_member(:etcd, {:url => 'http://127.0.0.1:2379/v2'})
 org.add_member(:lmdb, {:path => '/tmp/tmp.lmdb', :options => {:flags => MDB::NOSUBDIR}})
 org.add_member(:redis, {:host => 'localhost', :port => 6379})
+org.add_member(:k2hash, {:path => '/tmp/tmp.k2hash', :options => {:mode => 0666, :flags => K2Hash::NEWDB}})
 
 org.put_to :etcd, 'key', 'etcd'
 org.put_to :lmdb, 'key', 'lmdb'
 org.put_to :redis, 'key', 'redis'
+org.put_to :k2hash, 'key', 'k2hash'
 p org.get_from_all 'key'
 
 org.put_to_all 'key', 'value'
