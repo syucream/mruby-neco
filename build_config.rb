1e1cf930075f3ee5b1985ef12bab32a0988387c1
Ryo Okubo <syucream1031@gmail.com>
2017-01-09 13:15:45 +0900

    Fix trivials

--- build_config.rb
+++ build_config.rb
@@ -4,12 +4,9 @@ MRuby::Build.new do |conf|
   conf.gembox 'default'
 
   ## KVS mrbgems
-  # C and/or Ruby
   conf.gem :git => 'https://github.com/udzura/mruby-etcd.git'
   conf.gem :git => 'https://github.com/Asmod4n/mruby-lmdb.git'
   conf.gem :git => 'https://github.com/matsumotory/mruby-redis.git'
-  # C++
-  #enable_cxx_abi
   #conf.gem :git => 'https://github.com/syucream/mruby-k2hash.git'
 
   conf.gem '../mruby/mrbgems/mruby-neco'
